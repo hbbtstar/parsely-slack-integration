144f5737cfb4e1f122067f2edc04adda813a4736
Alexander Lourenco <alexander.lourenco@gmail.com>
2015-08-26 14:37:25 +0100

    initial commit

---
+++ config.py
@@ -0,0 +1,6 @@
+DAYS = 1 # days to check
+APIKEY = "elevatedtoday.com" # apikey
+SHARED_SECRET = "" # shared secret
+WEBHOOK_URL = "https://hooks.slack.com/services/abcdef" # your webhook URL given to you by Slack
+CHANNEL = "#general" # channel you want to send the API results to
+ROOT_URL = "https://api.parsely.com/v2/" # version of the parsely API to use (v2 is most recent as of this commit)
